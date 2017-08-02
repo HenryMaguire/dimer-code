Fixed the strange exponential scaling of the serial non-secular, I was accidentally looping over sorted(l) and l, rather than passing them as a tuple as the parfor function does

dimer_dynamics.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 
     w_2 = 1.4*ev_to_inv_cm
     V = 92 #0.01*8065.5
-    bias = 0 #1*V #0.01*ev_to_inv_cm
+    bias = V #1*V #0.01*ev_to_inv_cm
     w_1 = w_2 + bias
     dipole_1, dipole_2 = 1., 1.
     T_EM = 6000. # Optical bath temperature
@@ -58,11 +58,11 @@ if __name__ == "__main__":
     T_1, T_2 = 300., 300. # Phonon bath temperature
 
     wc = 1*53. # Ind.-Boson frame phonon cutoff freq
-    w0_2, w0_1 = 400., 400. # underdamped SD parameter omega_0
+    w0_2, w0_1 = 500., 500. # underdamped SD parameter omega_0
     w_xx = w_2 + w_1
-    alpha_1, alpha_2 = 200/pi, 200/pi # Ind.-Boson frame coupling
-    N_1, N_2 = 4,4 # set Hilbert space sizes
-    exc = 6
+    alpha_1, alpha_2 = 1/pi, 1/pi # Ind.-Boson frame coupling
+    N_1, N_2 = 3,3 # set Hilbert space sizes
+    exc = 2
     num_cpus = 4
     J = J_minimal
 
@@ -127,21 +127,14 @@ if __name__ == "__main__":
     opts = qt.Options(num_cpus=num_cpus, store_states=True)
     ncolors = len(plt.rcParams['axes.prop_cycle'])
 
-    #L_RC, H_0, A_1, A_2, A_EM, wRC_1, wRC_2, kappa_1, kappa_2 = RC.RC_mapping_UD(PARAMS)
-    #L_ns = EM.L_nonsecular(H_0, A_EM, PARAMS)
-    #L_s = EM.L_secular(H_0, A_EM, PARAMS)
-    print "L_s calculated"
-    #L_p = EM.L_phenom(states, energies, I, PARAMS)
     thermal_RCs = enr_thermal_dm([N_1,N_2], exc, [n_RC_1, n_RC_2])
     rho_0 = tensor(basis(4,0)*basis(4,0).dag(),thermal_RCs)
-    timelist = np.linspace(0,3,1000)
-
-    #DATA_p = mesolve(H_0, rho_0, timelist, [L_ns], expects, options=opts, progress_bar=True)
-    #DATA_ns = mesolve(H_0, rho_0, timelist, [L_RC+L_ns], expects, options=opts, progress_bar=True)
-    #try:
-    #    DATA_s = mesolve(H_0, rho_0, timelist, [L_RC+L_s], expects, options=opts, progress_bar=True)
-    #except Exception as e:
-    #    print "didn't work due to ", e
+    #timelist = np.linspace(0,3,1000)
+    L_RC, H_0, A_1, A_2, A_EM, wRC_1, wRC_2, kappa_1, kappa_2 = RC.RC_mapping_UD(PARAMS)
+    L_full_s = vis.calculate_dynamics(rho_0, L_RC, H_0, A_EM, expects, PARAMS, EM_approx='s')
+    L_full_p = vis.calculate_dynamics(rho_0, L_RC, H_0, A_EM, expects, PARAMS, EM_approx='p')
+    L_full_ns = vis.calculate_dynamics(rho_0, L_RC, H_0, A_EM, expects, PARAMS, EM_approx='ns')
+    
     """
     mut_inf_d1 = []
     mut_inf_d2 = []
@@ -160,22 +153,22 @@ if __name__ == "__main__":
     #ax1 = fig.add_subplot(211)
     #ax2 = fig.add_subplot(212)
     """
-    """
-    ax1.plot(timelist, DATA_ns.expect[7].real, label='real eig. coherence')
-    ax1.plot(timelist, DATA_ns.expect[7].imag, label='imag. eig. coherence')
-    ax2.plot(timelist, DATA_ns.expect[8]-DATA_ns.expect[9], label='Phonon occupation diff.')
+
+    #ax1.plot(timelist, DATA_ns.expect[7].real, label='real eig. coherence')
+    #ax1.plot(timelist, DATA_ns.expect[7].imag, label='imag. eig. coherence')
+    #ax2.plot(timelist, DATA_ns.expect[8]-DATA_ns.expect[9], label='Phonon occupation diff.')
     #ax2.plot(timelist, DATA_ns.expect[1], label='site 1', linestyle="--")
     #ax2.plot(timelist, DATA_ns.expect[2], label='site 2', linestyle="--")
     #ax2.plot(timelist, DATA_ns.expect[0]+ DATA_ns.expect[5]+ DATA_ns.expect[6]+ DATA_ns.expect[3], label='nsb')
-    ax1.plot(timelist, DATA_s.expect[7].real, linestyle="--",label='secular')
+    #ax1.plot(timelist, DATA_s.expect[7].real, linestyle="--",label='secular')
     #ax1.plot(timelist, DATA_s.expect[7].imag, linestyle="--", label='pbc imag')
     #ax2.plot(timelist, DATA_ns.expect[5], linestyle="-",label='dark', color ='r')
     #ax2.plot(timelist, DATA_ns.expect[6], linestyle="-", label='bright', color ='b')
     #ax2.plot(timelist, DATA_ns.expect[3], linestyle="-", label='exc')
     #ax2.plot(timelist, DATA_s.expect[5], linestyle="-",label='p dark')
     #ax2.plot(timelist, DATA_s.expect[6], linestyle="-", label='p bright')
-    method= 'direct' #'iterative-lgmres'
-    """
+    #method= 'direct' #'iterative-lgmres'
+
     """
     plt.figure()
     plt.plot(timelist, DATA_ns.expect[5], linestyle="-",label='dark', color ='r')
@@ -254,8 +247,7 @@ if __name__ == "__main__":
     #check.get_coh_ops(PARAMS, biases, I)
     #
     """
-
-
+    """
     alpha_ph = np.array([0.1, 1., 10., 100., 500.])/pi
     #alpha_ph=np.array([0])
     biases = np.linspace(0, 0.03, 50)*ev_to_inv_cm
@@ -350,7 +342,7 @@ if __name__ == "__main__":
     vis.steadystate_dark_plot(PARAMS, alpha_ph, biases)
     vis.steadystate_bright_plot(PARAMS, alpha_ph, biases)'''
     vis.steadystate_darkbright_plot(PARAMS, alpha_ph, biases)
-
+    """
     #del L_ns
     #L_s = EM.L_secular(H_0, A_EM, eps, alpha_EM, T_EM, J, num_cpus=num_cpus)
     #L_naive = EM_lind.electronic_lindblad(w_xx, w_1, eps, V, mu, alpha_EM, T_EM, N_1, N_2, exc)

dimer_optical.py
@@ -52,6 +52,7 @@ def secular_function(i,j, eVals=[], eVecs=[], A=0, w_1=8000., Gamma=1.,T=0., J=J
     lam_ij_sq = lam_ij*lam_ij.conjugate()
     eps_ij = abs(eVals[i]-eVals[j])
     if lam_ij_sq>0:
+        print lam_ij_sq
         IJ = eVecs[i]*eVecs[j].dag()
         JI = eVecs[j]*eVecs[i].dag()
         JJ = eVecs[j]*eVecs[j].dag()
@@ -83,30 +84,29 @@ def L_nonsecular(H_vib, A, args):
     eVals, eVecs = H_vib.eigenstates()
     l = dim_ham*range(dim_ham) # Perform two loops in one
     X1, X2, X3, X4 = 0,0,0,0
-    for i in sorted(l):
-        for j in l:
-            eps_ij = abs(eVals[i]-eVals[j])
-            A_ij = A.matrix_element(eVecs[i].dag(), eVecs[j])
-            A_ji = (A.dag()).matrix_element(eVecs[j].dag(), eVecs[i])
-            Occ = Occupation(eps_ij, T)
+    for i,j in zip(sorted(l), l):
+        eps_ij = abs(eVals[i]-eVals[j])
+        A_ij = A.matrix_element(eVecs[i].dag(), eVecs[j])
+        A_ji = (A.dag()).matrix_element(eVecs[j].dag(), eVecs[i])
 
-            # 0.5*np.pi*alpha*(N+1)
-            if abs(A_ij)>0 or abs(A_ji)>0:
-                IJ = eVecs[i]*eVecs[j].dag()
-                JI = eVecs[j]*eVecs[i].dag()
-                r_up = 0
-                r_down = 0
-                if eps_ij == 0:
-                    JN = Gamma/(2*pi*w_1*beta_f(T))
-                    r_up = 2*pi*JN
-                    r_down = 2*pi*JN
-                else:
-                    r_up = 2*pi*J(eps_ij, Gamma, w_1)*Occ
-                    r_down = 2*pi*J(eps_ij, Gamma, w_1)*(Occ+1)
-                X3+= r_down*A_ij*IJ
-                X4+= r_up*A_ij*IJ
-                X1+= r_up*A_ji*JI
-                X2+= r_down*A_ji*JI
+        # 0.5*np.pi*alpha*(N+1)
+        if abs(A_ij)>0 or abs(A_ji)>0:
+            Occ = Occupation(eps_ij, T)
+            IJ = eVecs[i]*eVecs[j].dag()
+            JI = eVecs[j]*eVecs[i].dag()
+            r_up = 0
+            r_down = 0
+            if eps_ij == 0:
+                JN = Gamma/(2*pi*w_1*beta_f(T))
+                r_up = 2*pi*JN
+                r_down = 2*pi*JN
+            else:
+                r_up = 2*pi*J(eps_ij, Gamma, w_1)*Occ
+                r_down = 2*pi*J(eps_ij, Gamma, w_1)*(Occ+1)
+            X3+= r_down*A_ij*IJ
+            X4+= r_up*A_ij*IJ
+            X1+= r_up*A_ji*JI
+            X2+= r_down*A_ji*JI
     L = spre(A*X1) -sprepost(X1,A)+spost(X2*A)-sprepost(A,X2)
     L+= spre(A.dag()*X3)-sprepost(X3, A.dag())+spost(X4*A.dag())-sprepost(A.dag(), X4)
     #print np.sum(X1.full()), np.sum(X2.full()), np.sum(X3.full()), np.sum(X4.full())

dimer_phonons.py
@@ -152,7 +152,7 @@ def liouvillian_build(H_0, A_1, A_2, gamma_1, gamma_2,  wRC_1, wRC_2, T_1, T_2, 
         beta_2 = 1./(conversion * T_2)
         #RCnb_2 = (1 / (sp.exp( beta_2 * wRC_2)-1))
     # Now this function has to construct the liouvillian so that it can be passed to mesolve
-    H_0, Chi_1, Xi_1,Chi_2, Xi_2  = RCME_operators(H_0, A_1, A_2, gamma_1, gamma_2, beta_1, beta_2, num_cpus=num_cpus)
+    H_0, Chi_1, Xi_1,Chi_2, Xi_2  = RCME_operators_par(H_0, A_1, A_2, gamma_1, gamma_2, beta_1, beta_2, num_cpus=num_cpus)
     #H_0, Chi_1_, Xi_1_,Chi_2_, Xi_2_  = RCME_operators(H_0, A_1, A_2, gamma_1, gamma_2, beta_1, beta_2)
     '''
     print np.sum(Chi_1.full() == Chi_1_.full()), Chi_1.shape[0]**2

dimer_plotting.py
@@ -1,7 +1,8 @@
 """
 Plotting module for the dimer dynamics
 """
-
+import dimer_phonons as RC
+import dimer_optical as EM
 from qutip import steadystate, Qobj
 import qutip as qt
 import matplotlib.pyplot as plt
@@ -210,15 +211,39 @@ def plot_observable(DATA, timelist, ax, things):
         if things['show_steadystates']:
             ax.axhline(SS)
 
+def exciton_states(PARS):
+    w_1, w_2, V, bias = PARS['w_1'], PARS['w_2'],PARS['V'], PARS['bias']
+    v_p, v_m = 0, 0
+    eta = np.sqrt(4*(V**2)+bias**2)
+    lam_p = w_2+(bias+eta)*0.5
+    lam_m = w_2+(bias-eta)*0.5
+    v_m = np.array([0., -(w_1-lam_p)/V, -1, 0.])
+    #v_p/= /(1+(V/(w_2-lam_m))**2)
+    v_m/= np.sqrt(np.dot(v_m, v_m))
+    v_p = np.array([0, V/(w_2-lam_m),1., 0.])
 
-def calculate_dynamics():
-    assert PARAMS['w_1'] != PARAMS['w_2']
+    v_p /= np.sqrt(np.dot(v_p, v_p))
+    #print  np.dot(v_p, v_m) < 1E-15
+    return [lam_m, lam_p], [qt.Qobj(v_m), qt.Qobj(v_p)]
+
+def calculate_dynamics(rho_0, L_RC, H_0, A_EM, expects, PARAMS, EM_approx='s'):
+    L=0
     try:
-        timelist = np.linspace(0,20.0,5000)*0.188
-        L_ns = EM.L_nonsecular(H_0, A_EM, PARAMS)
-        L_full = L_RC+L_ns
+        if EM_approx=='ns':
+            L = EM.L_nonsecular(H_0, A_EM, PARAMS)
+        elif EM_approx=='s':
+            L = EM.L_secular(H_0, A_EM, PARAMS)
+        elif EM_approx=='p':
+            I = qt.enr_identity([PARAMS['N_1'],PARAMS['N_2']], PARAMS['exc'])
+            energies, states = exciton_states(PARAMS)
+            L = EM.L_phenom(states, energies, I, PARAMS)
+        else:
+            raise KeyError
+        timelist = np.linspace(0,20.0,5000)
 
-        DATA_ns = mesolve(H_0, rho_0, timelist, [L_full], expects, options=opts, progress_bar=True)
+        L_full = L_RC+L
+        opts = qt.Options(num_cpus=4, store_final_state=True)
+        DATA = qt.mesolve(H_0, rho_0, timelist, [L_full], expects, progress_bar=True, options=opts)
         ss_dm = 0
         try:
             ss_dm = qt.steadystate(H_0, [L_full])
@@ -227,23 +252,22 @@ def calculate_dynamics():
             print "\t due to some problem with excitation restriction. \n"
             print err
 
-        timelist=timelist/0.188 # Convert from cm to picoseconds
+        #timelist=timelist/0.188 # Convert from cm to picoseconds
         #DATA_ns = load_obj("DATA_N7_exc8")
         #fig = plt.figure(figsize=(12,6))
         fig = plt.figure()
         ax1 = fig.add_subplot(111)
-        title = 'Eigenstate population'
+        title = 'Eigenstate dynamics'
         #title = title + r"$\omega_0=$""%i"r"$cm^{-1}$, $\alpha_{ph}=$""%f"r"$cm^{-1}$, $T_{EM}=$""%i K" %(w0_1, alpha_1, T_EM)
         fig = plt.figure()
-        vis.plot_eig_dynamics(DATA_ns, timelist, expects, ax1, ss_dm=ss_dm)
+        plot_eig_dynamics(DATA, timelist, expects, ax1, ss_dm=ss_dm)
         ax2 = fig.add_subplot(111)
-        vis.plot_coherences(DATA_ns, timelist, expects, ax2, ss_dm=ss_dm)
-        plt.savefig("Notes/dynamics.png")
-        print (ss_dm*exciton_coherence).tr()
-        print 'Plotting worked!'
+        plot_coherences(DATA, timelist, expects, ax2, ss_dm=ss_dm)
+        plt.savefig("DATA/{}_dynamics.png".format(EM_approx))
+        print 'Plotting finished!'
         return L_full
     except Exception as err:
-        print "Could not get non-secular-driving dynamics because ",err
+        print "Could not get dynamics because ",err
 
 def steadystate_coherence_plot(args, alpha_list, biases):
     main_dir = "DATA/bias_dependence_wRC{}_N{}_V{}_wc{}/".format(int(args['w0_1']), args['N_1'], int(args['V']), int(args['wc']))