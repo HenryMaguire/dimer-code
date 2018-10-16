3a02cfc8ccc6e86d73841e4698289d41d2244ea0
HenryMaguire <henrymaguire@hotmail.co.uk>
2018-10-16 15:30:02 +0100

    tweaked the utils file and the imports into the notebooks

--- utils.py
+++ utils.py
@@ -269,3 +269,32 @@ def make_expectation_operators(PARS):
 
 
     return dict((key_val[0], key_val[1]) for key_val in zip(labels, fullspace_ops))
+
+
+# Sparse matrix utilities
+
+from scipy.sparse import csc_matrix
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def total_elements(qobj):
+    return qobj.shape[0]**2
+
+def nonzero_elements(qobj):
+    return len(csc_matrix(qobj.data).nonzero()[0])
+
+def visualise_dense(qobj):
+    plt.imshow(qobj.full().real)
+    plt.show()
+
+def visualise_sparse(qobj):
+    plt.spy(qobj.data)
+    plt.show()
+
+import copy
+def chop(X, threshold=1e-7):
+    _X = copy.deepcopy(X)
+    _X.tidyup(threshold)
+    return _X
